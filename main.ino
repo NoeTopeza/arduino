e21abdc907f125a6bf58307d0cd936681335fdc7
NoeTopeza <35497071+NoeTopeza@users.noreply.github.com>
2018-04-11 00:26:31 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -3,12 +3,15 @@
 
 #include "Arduino.h"
 
-// the setup function runs once when you press reset or power the board
 void setup()
 {
+  pinMode(13, OUTPUT);
 }
 
-// the loop function runs over and over again forever
 void loop()
 {
+  digitalWrite(13, HIGH);
+  delay(2000);
+  digitalWrite(13, LOW);
+  delay(2000);
 }
