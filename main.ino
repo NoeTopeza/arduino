a9e5f492b6dd1fec6a02f407e314a10a1fe9836d
NoeTopeza <35497071+NoeTopeza@users.noreply.github.com>
2018-04-11 13:10:08 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -10,8 +10,11 @@ void setup()
 
 void loop()
 {
+  static int potPosition;
+  
+  potPosition = analogRead(A0);
   digitalWrite(13, HIGH);
-  delay(2000);
+  delay(potPosition);
   digitalWrite(13, LOW);
-  delay(2000);
+  delay(potPosition);
 }
